4a389e46554b24e900ebeac2fdd673ce07d9c5b0
LeonardToro97 <leonardtoro06.fgltq@gmail.com>
2023-04-03 13:04:10 -0500

    12

--- crearEntornoNode.sh
+++ crearEntornoNode.sh
@@ -23,7 +23,7 @@ echo "Archivos copiados."
 cd ${PROJECT_FOLDER}
 
 echo "Creando entorno virtual de Node.js..."
-python -m nodeenv --node="${NODE_VERSION}" env
+python3 -m nodeenv --node="${NODE_VERSION}" env
 echo "Entorno virtual de Node.js creado."
 
 echo "Activando entorno virtual de Node.js..."
