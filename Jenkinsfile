27c86f13a709db3b587851ac93933d6f576ebf9d
LeonardToro97 <leonardtoro06.fgltq@gmail.com>
2023-04-03 12:06:51 -0500

    Mi tercer commit

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,12 @@
 pipeline {
     agent any
 
+    parameters {
+        string(name: 'INDEX_FILE', defaultValue: 'index.js', description: 'Nombre del archivo index.js a utilizar.')
+        string(name: 'PACKAGE_FILE', defaultValue: 'package.json', description: 'Nombre del archivo package.json a utilizar.')
+        string(name: 'NODE_VERSION', defaultValue: '14.17.0', description: 'Versión de Node.js a utilizar.')
+    }
+
     stages {
         stage('Preparar entorno') {
             steps {
@@ -11,7 +17,7 @@ pipeline {
 
         stage('Crear entorno y ejecutar script') {
             steps {
-                sh './crearEntornoNode.sh'
+                sh "./crearEntornoNode.sh"
             }
         }
     }
--- crearEntornoNode.sh
+++ crearEntornoNode.sh
@@ -1,42 +1,34 @@
 #!/bin/bash
 
-project_folder_base="mi_proyecto"
-project_folder="$project_folder_base"
-i=0
+PROJECT_FOLDER_BASE="mi_proyecto"
+PROJECT_FOLDER="${PROJECT_FOLDER_BASE}"
 
-while [ -e "$project_folder" ]; do
-  i=$((i + 1))
-  project_folder="${project_folder_base}_${i}"
-done
+if [ -d "${PROJECT_FOLDER}" ]; then
+    i=1
+    while [ -d "${PROJECT_FOLDER}_${i}" ]; do
+        ((i++))
+    done
+    PROJECT_FOLDER="${PROJECT_FOLDER}_${i}"
+fi
 
-echo "Creando carpeta del proyecto: $project_folder..."
-mkdir "$project_folder"
+echo "Creando carpeta del proyecto: ${PROJECT_FOLDER}..."
+mkdir ${PROJECT_FOLDER}
 echo "Carpeta del proyecto creada."
 
-if [ $i -eq 0 ]; then
-  echo "Obteniendo la versión de Node.js..."
-  node_version=$(cat config_files/node_version.txt)
-  echo "Versión de Node.js obtenida: $node_version."
-  echo "Copiando package.json e index.js a la carpeta del proyecto..."
-  cp config_files/package.json "$project_folder"
-  cp config_files/index.js "$project_folder"
-else
-  echo "Obteniendo la versión de Node.js..."
-  node_version=$(cat "config_files/node_version_${i}.txt")
-  echo "Versión de Node.js obtenida: $node_version."
-  echo "Copiando package_${i}.json e index_${i}.js a la carpeta del proyecto..."
-  cp "config_files/package_${i}.json" "$project_folder/package.json"
-  cp "config_files/index_${i}.js" "$project_folder/index.js"
-fi
+echo "Copiando ${PACKAGE_FILE} e ${INDEX_FILE} a la carpeta del proyecto..."
+cp "config_files/${PACKAGE_FILE}" "${PROJECT_FOLDER}/package.json"
+cp "config_files/${INDEX_FILE}" "${PROJECT_FOLDER}/index.js"
 echo "Archivos copiados."
 
-cd "$project_folder"
+cd ${PROJECT_FOLDER}
 
 echo "Creando entorno virtual de Node.js..."
-python -m nodeenv --node="$node_version" env
+nodeenv --node=${NODE_VERSION} env
 echo "Entorno virtual de Node.js creado."
 
 echo "Activando entorno virtual de Node.js..."
 source env/bin/activate
-chmod +x ../ejecutarScript.sh
-../ejecutarScript.sh
+echo "Entorno virtual de Node.js activado."
+
+./ejecutarScript.sh
+
