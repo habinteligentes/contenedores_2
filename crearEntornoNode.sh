317310e0e9c679d7a0e485c6e3e5328eec5c0081
LeonardToro97 <leonardtoro06.fgltq@gmail.com>
2023-04-03 15:04:45 -0500

    1

--- crearEntornoNode.sh
+++ crearEntornoNode.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
 PROJECT_FOLDER_BASE="mi_proyecto"
-PROJECT_FOLDER="${PROJECT_FOLDER_BASE}"
+PROJECT_FOLDER="$PROJECT_FOLDER_BASE"
+i=0
 
-if [ -d "${PROJECT_FOLDER}" ]; then
-    i=1
-    while [ -d "${PROJECT_FOLDER}_${i}" ]; do
-        ((i++))
-    done
-    PROJECT_FOLDER="${PROJECT_FOLDER}_${i}"
-fi
+while [ -e "$PROJECT_FOLDER" ]; do
+  i=$((i + 1))
+  PROJECT_FOLDER="${PROJECT_FOLDER_BASE}_${i}"
+done
 
-echo "Creando carpeta del proyecto: ${PROJECT_FOLDER}..."
-mkdir ${PROJECT_FOLDER}
+echo "Creando carpeta del proyecto: $PROJECT_FOLDER..."
+mkdir "$PROJECT_FOLDER"
 echo "Carpeta del proyecto creada."
 
 echo "Copiando ${PACKAGE_FILE} e ${INDEX_FILE} a la carpeta del proyecto..."
-cp "config_files/${PACKAGE_FILE}" "${PROJECT_FOLDER}/package.json"
-cp "config_files/${INDEX_FILE}" "${PROJECT_FOLDER}/index.js"
+cp "config_files/${PACKAGE_FILE}" "$PROJECT_FOLDER/package.json"
+cp "config_files/${INDEX_FILE}" "$PROJECT_FOLDER/index.js"
 echo "Archivos copiados."
 
-cd ${PROJECT_FOLDER}
+cd "$PROJECT_FOLDER"
 
 echo "Creando entorno virtual de Node.js..."
 
-pip3 install nodeenv --user
 
-~/.local/bin/nodeenv --node="${NODE_VERSION}" env
-
+python -m nodeenv --node="${NODE_VERSION}" env
 
 echo "Entorno virtual de Node.js creado."
-
-echo "Activando entorno virtual de Node.js..."
-source env/bin/activate
-echo "Entorno virtual de Node.js activado."
 
 echo "Contenido de la carpeta actual:"
 ls
 
-./ejecutarScript.sh
+echo "Activando entorno virtual de Node.js..."
+source env/bin/activate
+chmod +x ../ejecutarScript.sh
+../ejecutarScript.sh
 
