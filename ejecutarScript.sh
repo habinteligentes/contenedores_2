7251546abc24caba779de64f87222dee9dca194c
LeonardToro97 <leonardtoro06.fgltq@gmail.com>
2023-04-03 11:38:35 -0500

    Mi segundo commit

--- ejecutarScript.sh
+++ ejecutarScript.sh
@@ -3,6 +3,9 @@
 echo "Ejecutando el script en el entorno virtual..."
 node index.js
 
+echo "Mostrando paquetes instalados en el entorno virtual..."
+npm list
+
 echo "Desactivando entorno virtual de Node.js..."
 deactivate_node
 echo "Entorno virtual de Node.js desactivado."
